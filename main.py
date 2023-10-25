c969347e5917eae5e93ed2a9fcbc35bf30e03f8f
Ludovic Dupre <dupreludovic1@gmail.com>
2023-10-25 08:55:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+aircraft_carrier = ((2, 2, True), (2, 3, True), (2, 4, True), (2, 5, True), (2, 6, True))
+cruiser  = ((4,1, True ),(5,1, True),(6,1, True),(7,1,True))
+destroyer = ((5,3,True),(6,3,True),(7,3,True))
+submarine =((5,7,True),(5,8,True),(5,9,True))
+torpedo_boat = ((9,5,True),(9,6,True))
+ships_list = [aircraft_carrier, cruiser, destroyer, submarine, torpedo_boat]
